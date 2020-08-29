f72ee0004a15a6c6892f88c01859137bb979d212
hungtran <trhun810@gmail.com>
2020-08-29 21:59:48 +0700

    First commit

--- Day2/index.js
+++ Day2/index.js
@@ -1,11 +1,13 @@
 let a = prompt("Enter a number:");
-
+let sum = 0;
 for(let i = 1; i <= a ; i++ )  {
   console.log(i);
+  sum += i;
 }
+alert(sum);
 
-let password = prompt('Enter a password');
-while( password.length <= 6 || password.length >= 20) {
-  password = prompt('Rename?');
-}
-console.log(password);
+// let password = prompt('Enter a password');
+// while( password.length <= 6 || password.length >= 20) {
+//   password = prompt('Rename?');
+// }
+// console.log(password);
