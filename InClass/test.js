83b8eaa477e745b79f92ff886799e7ff971d061c
hungtran <trhun810@gmail.com>
2020-09-01 21:47:47 +0700

    Second fix

--- InClass/test.js
+++ InClass/test.js
@@ -1,4 +1,26 @@
-var first;
-    let name = prompt('Enter a fruit?');
-    first.push(name);
-console.log(first);
+const n_bills = Number(prompt('Enter n bills:'));
+const TotalBill = [];
+const Tip = [];
+for(let i = 0; i < n_bills; i++){
+    TotalBill.push(Number(prompt(`Enter the price of bill ${i+1}:`)));
+    while(TotalBill[TotalBill.length-1] <= 0){
+        TotalBill.pop();
+        TotalBill.push(Number(prompt(`Enter the price of bill ${i+1}:`)));  
+    }
+}
+for(let i = 0; i < n_bills; i++){
+    if(TotalBill[i] < 50) {
+        Tip[i] = TotalBill[i] * 0.2;
+    }
+    else if(TotalBill[i] < 200) {
+        Tip[i] = TotalBill[i] * 0.15;
+    }
+    else Tip[i] = TotalBill[i] * 0.1;
+}
+
+for(let i = 0; i < n_bills; i++){
+    confirm(Tip[i]);
+}
+for(let i = 0; i < n_bills; i++){
+    confirm(TotalBill[i]+Tip[i]);
+}
