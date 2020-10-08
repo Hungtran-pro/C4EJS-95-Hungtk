f1c82a3b336e27b8d8ffe3508ab1c7e197a60cf8
hungtran <trhun810@gmail.com>
2020-10-08 19:20:03 +0700

    first

---
+++ Final_Project/data.js
@@ -0,0 +1,31 @@
+let House = [
+  {
+    title: "Nhà Tây",
+    price: 7000,
+    bedNumber: 5,
+    bathNumber: 5,
+    car: true,
+    floors: 3,
+    address: "Ho Tay",
+  },
+  {
+    title: "Nhà Ta",
+    price: 7000,
+    bedNumber: 5,
+    bathNumber: 5,
+    car: true,
+    floors: 3,
+    address: "Ho Tay",
+  },
+  {
+    title: "Nhà Đẹp",
+    price: 7000,
+    bedNumber: 5,
+    bathNumber: 5,
+    car: true,
+    floors: 3,
+    address: "Ho Tay",
+  },
+];
+
+console.log(listData);
--- InClass/test.js
+++ InClass/test.js
@@ -85,4 +85,20 @@
 //     console.log("Yay! " + successMessage) 
   // });
 
+  function resolveAfter2Seconds() {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve('resolvjkjkhked');
+      }, 2000);
+    });
+  }
+  
+  async function asyncCall() {
+    console.log('calling');
+    const result = await resolveAfter2Seconds();
+    console.log(result);
+    // expected output: "resolved"
+  }
+  
+  asyncCall();
   
